f06d9514836e90488541a4c92a1dfbb5248fab6f
huyao <2385249676@qq.com>
2018-05-22 22:47:19 +0800

    弹窗

---
+++ 弹窗/demo.js
@@ -0,0 +1,8 @@
+$('#share').on('click', function () {
+    $('#shareBox').show();
+    $('#shade').show();
+});
+$('#close').on('click', function () {
+    $('#shareBox').hide();
+    $('#shade').hide();
+});
