69b67ea72608879256bb78cc581d072f386e5c9e
huyao <2385249676@qq.com>
2018-05-27 23:31:16 +0800

    数字时钟-功能完成

---
+++ 数字时钟/demo.js
@@ -0,0 +1,58 @@
+// 1. 获得当前时间
+//  22:33:45 -> 字符串
+// 2. 当前时间垂直居中
+//  不是当前时间的数字-> 根据数字远近  opacity ->切换透明度
+
+// 创建star函数
+
+// getClock函数获得时间
+
+//数组reduce方法
+/**
+ * TimeClock类
+ * @param {操纵的dom} dom 
+ * @param {是否使用24小时制} use24Hours 
+ */
+function TimeClock(dom, use24Hours){
+    this.colums = Array.from(dom);
+    this.use24Hours = use24Hours;
+    this.classList = ['visible','colse','far','distant'];
+    this.start();
+}
+
+/**
+ * 先得到时间
+ * 按周期进行位置调整和CSS样式改变
+ */
+TimeClock.prototype.start = function(){
+    var self = this;
+    setInterval(function(){
+        var now = self.getClock();
+        self.colums.forEach(function(ele, index){
+            // var num = +now[index],
+            //     offset = num * 86 + 43;
+            // $(ele).css({
+            //     'transform': 'translateY(calc(50vh - '+ offset +'px))'
+            // })
+            var n = + now[index]; // + 把字符串转为数字
+        var offset = n * 86;
+        $(ele).css({
+            'transform': 'translateY(calc(50vh - ' + offset + 'px - ' + 43 + 'px))'
+        })
+        })
+    }, 200);
+}
+
+/**
+ * 获取时间字符串
+ * 比如当前时间 13:34:6， 返回 133406
+ */
+TimeClock.prototype.getClock = function(){
+
+    var now = new Date();
+    function getTimeStr(total, num){
+        return total + ('0' + num).slice(-2);
+    }
+    return [now.getHours(), now.getMinutes(), now.getSeconds()].reduce(getTimeStr, '');
+}
+new TimeClock($('.column'), true)
