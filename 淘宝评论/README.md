84e9b97b4fccd5e23ec1c560abce9569b6b8d84d
huyao <2385249676@qq.com>
2018-06-03 18:30:06 +0800

    淘宝评论

---
+++ 淘宝评论/README.md
@@ -0,0 +1,15 @@
+# 淘宝评论
+
+## 效果展示
+![demo](./demo.gif)
+
+## 三角形的绘制
+```css
+.tri{
+    display: inline-block;
+    width: 0;
+    height: 0;
+    border: 6px solid transparent;
+    border-top:  6px solid #f23d6a;
+}
+```
--- 淘宝评论/demo.js
+++ 淘宝评论/demo.js
@@ -4,45 +4,45 @@
 
 init();
 function init() {
-    start();
+    bindLiEvnet();
     bindLeftRight();
 }
 /**
  * 点击图片 -> 大图展示，样式改变
  * 再次点击 -> 图片关闭
  */
-function start() {
+function bindLiEvnet() {
     $('li').on('click', function(){
         var $this = $(this);
         $this.toggleClass('active').siblings().removeClass('active');
-        var src = $this.find('img').attr('src');    //获得图片的src
-        var img = new Image();
-        img.src = src;
-        $photoView = $('.photoView');
-        img.onload = function(){
-            $photoView.css({
-                'height': img.height + 'px',
-                'width': '480px',
-            })
-            $photoView.on('click',function(){
-                $this.removeClass('active');
+        var src = $this.find('img').attr('src'),    //获得图片的src
+            flag = $this.hasClass('active'),
+            img = new Image(),
+            $photoView = $('.photoView');
+        if(flag){
+            img.src = src;
+            img.onload = function(){
                 $photoView.css({
-                    'width': 0,
-                    'height': 0
+                    'height': img.height + 'px',
+                    'width': '480px',
                 })
+                $photoView.on('click',function(){
+                    $this.removeClass('active');
+                    $photoView.css({
+                        'width': 0,
+                        'height': 0
+                    })
+                })
+            }
+            $photoView.find('img').attr('src', src);
+        } else {
+            $photoView.css({
+                'width': 0 + 'px',
+                'height': 0 + 'px',
+                'transition': 'all 100ms ease-out'
             })
         }
-        $photoView.find('img').attr('src', src);
-    })
-}
-function bindLeftRight(){
-    $('.photoView a').on('click', function(){
-        var $this = $(this),
-            clickTar = {};
-        clickTar.direction = $this.attr('class');
-        clickTar.index = $('.photos').find('li').index($('li.active'));
-        var index = clickTar.index;
-        console.log(clickTar);
+        
     })
 }
 /**
@@ -52,3 +52,36 @@ function bindLeftRight(){
  * 更换展示图片
  * 更改选中的小图
  */
+function bindLeftRight(){
+    
+    $('.photoView a').on('click', function(){
+        var clickTar = {};
+        var $this = $(this),
+            len = $('#photos').find('li').length,
+            activeLi = $('#photos').find('li.active');
+        clickTar.direction = $this.attr('class');
+        clickTar.index = $('#photos').find('li').index($('li.active'));
+        var index = clickTar.index;
+        console.log(clickTar)
+        clickTar.direction == 'left' ? index-- : index++;
+
+        if(index >= 0 && index < len) {
+
+            $('#photos').find('li').eq(index).attr('class', 'active').siblings().attr('class', '');
+            var src = $('#photos').find('li').eq(index).find('img').attr('src');
+            $('.photoView').find('img').attr("src", src);
+            var img = new Image();
+            img.src = src;
+            img.onload = function(){
+                $('.photoView').css({
+                    'height': img.height + 'px',
+                    'width': '480px'
+                })
+            }
+            $('.photoView').find('img').attr('src', src);
+        } else {
+            console.log('remove')
+            $('#photos').find('li').toggleClass('active').siblings().removeClass('active');
+        }
+    })
+}
